daaec3f8f52cf9bc3d3f1173113063eaf5d69341
최지수 <cjscoding1997@gmail.com>
2022-03-28 13:32:38 +0900

    [S06P22C107-139][S06P22C107-140]Feat: 작가 작품, 작가 세부정보 조회 API 구현

--- frontend/store/modules/index.ts
+++ frontend/store/modules/index.ts
@@ -4,12 +4,14 @@ import counterReducer from "./counter";
 import loginReducer from "./login";
 import navBarReducer from "./navbar";
 import itemReducer from "./item";
+import artistReducer from "./artist";
 
 const rootReducer = combineReducers({
   counterReducer,
   loginReducer,
   navBarReducer,
   itemReducer,
+  artistReducer,
   // 모듈 추가시 여기에 추가
 });
 
