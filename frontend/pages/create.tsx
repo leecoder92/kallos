91612deb017a3c3d31e0bd3d80c7514168f31323
helen121027 <helen121027@gmail.com>
2022-03-29 13:48:09 +0900

    [S06P22C107-69]feat : 등록 구현

--- frontend/pages/create.tsx
+++ frontend/pages/create.tsx
@@ -1,86 +1,116 @@
-import React, { useState, useEffect } from "react";
-import {
-  Box,
-  Button,
-  Container,
-  Stack,
-  TextField,
-  Typography,
-  Dialog,
-  DialogContent,
-  DialogActions,
-  CssBaseline,
-  ButtonBase,
-} from "@mui/material";
+import React, { useState, useEffect, useRef } from "react";
+import Web3 from "web3";
+import { create, IPFSHTTPClient } from 'ipfs-http-client'
+import { Box, Button, Container, Stack, TextField, Typography, Dialog, DialogContent, DialogActions, CssBaseline } from "@mui/material";
 import defaultImage from "../public/images/default-image.jpg";
 import Image from "next/image";
-import { create as ipfsHttpClient } from "ipfs-http-client";
-import { mintKallosTokenContract } from "web3Config";
+import Link from "next/link";
+import { mintKallosTokenContract, web3} from "web3Config";
 
-const client = ipfsHttpClient("https://ipfs.infura.io:5001/api/v0");
+interface MainProps {
+  account: string;
+}
 
-export default function Create({ account }) {
-  const [fileUrl, setFileUrl] = useState(null);
-  const [formInput, updateFormInput] = useState({
-    title: "",
-    description: "",
-  });
+// ipfs request
+const ipfsClient = require('ipfs-http-client');
+const projectId = '26r91y8NYOlD5hMX5CYsWQ1qFuc';
+const projectSecret = 'de18b82921385c7f06cc1dc10bba3229';
+const auth =
+    'Basic ' + Buffer.from(projectId + ':' + projectSecret).toString('base64');
 
-  // modal
-  const [open, setOpen] = React.useState(false);
-  const handleOpen = () => {
-    setOpen(true);
-  };
-  const handleClose = () => {
-    setOpen(false);
-  };
+const client = ipfsClient.create({
+    host: 'ipfs.infura.io',
+    port: 5001,
+    protocol: 'https',
+    headers: {
+        authorization: auth,
+    },
+});
 
-  //이미지 미리보기
-  const [image, setImage] = useState({
-    image_file: "",
-    preview_URL: defaultImage,
-  });
+client.pin.add('QmeGAVddnBSnKc1DLE7DLV9uuTqo5F7QbaveTjr45JUdQn').then((res) => {
+    console.log(res);
+});
 
-  const [loaded, setLoaded] = useState(false);
+const Create = ({ account }) =>  {
+  const [privateKey, setPrivateKey] = useState<string>('');
+  // console.log(account)
 
-  let inputRef;
-
-  // const saveImage = (e) => {
-  //   e.preventDefault();
-  //   const fileReader = new FileReader();
-
-  //   if (e.target.files[0]) {
-  //     setLoaded("loading");
-  //     fileReader.readAsDataURL(e.target.files[0]);
-  //   }
-  //   fileReader.onload = () => {
-  //     setImage({
-  //       image_file: e.target.files[0],
-  //       preview_URL: fileReader.result,
-  //     });
-  //     setLoaded(true);
-  //   };
-  // };
-
-  async function onChange(e) {
-    const file = e.target.files[0];
+  //이미지 미리보기 
+    const [image, setImage] = useState({
+      image_file: "",
+      preview_URL: defaultImage,
+    });
+    const [loaded, setLoaded] = useState(false);
+    const [fileUrl, setFileUrl] = useState('');
+    let inputRef:any;
+    // console.log(image);
+    
+    //IPFS  
+    let ipfs: IPFSHTTPClient | undefined;
     try {
-      const added = await client.add(file, {
-        progress: (prog) => console.log(`received: ${prog}`),
+      ipfs = create({
+        url: "https://ipfs.infura.io:5001/api/v0",
+  
       });
-      const url = `https://ipfs.infura.io/ipfs/${added.path}`;
-      setFileUrl(url);
-      console.log("file url:", fileUrl);
     } catch (error) {
-      console.log("Error uploading file: ", error);
+      console.error("IPFS error ", error);
+      ipfs = undefined;
     }
-  }
-
+    // url 받아오기
+    const saveImage = async (e) => {
+      e.preventDefault();
+      const fileReader = new FileReader();
+      
+      if(e.target.files[0]){
+        fileReader.readAsDataURL(e.target.files[0])
+      }
+      let new_image;
+      fileReader.onload = () => {
+        new_image = fileReader.result
+        setImage(
+          {
+            image_file: e.target.files[0],
+            preview_URL: new_image
+          }
+          )
+        setLoaded(true);
+      }
+      let updateFileUrl;
+      try {
+        const added = await ipfs.add(e.target.files[0]);
+        const url = `https://ipfs.infura.io/ipfs/${added.path}`;
+        setFileUrl(url);
+        console.log(url)
+      } catch (error) {
+        console.log("Error uploading file: ", error);
+      }
+    }
+    
+    //버튼누르면 metadata 생성
+  //   const [isMint, setIsMint ] = useState(false);
+  //   // 등록하기 변수 아이템(파일, 이름, input 클릭 참조), 작가명, 제목, 키워드, 토큰 ID
+  //   const [tokenId, setTokenId] = useState<string>('');
+  //   const ButtonCreate = async () => {
+  //   const { author, title, description } = formInput;
+  //   if (!author || !description || !title || !fileUrl) return;
+  //   const data = JSON.stringify({ author, description, title, image: fileUrl});
+  //   try {
+    //     const added = await ipfs.add(data);
+    //     const url = `https://ipfs.infura.io/ipfs/${added.path}`;
+    //     console.log(data)
+    //   } catch(error) {
+      //     console.log("Error uploading file: ", error);
+      //   }
+      //   setIsMint(true);
+      // }
+      
+  const [ formInput, updateFormInput ] = useState({ author : "", title:"", description:""});
   const createMint = async () => {
-    const { title, description } = formInput;
-    if (!title || !description || !fileUrl) return console.log("값이 비어있음");
+    const { author, title, description} = formInput;
+    if (!author || !title || !description || !fileUrl) return console.log("값이 비어있음");
 
     const data = JSON.stringify({
+      author,
       title,
       description,
       image: fileUrl,
@@ -96,120 +126,84 @@ export default function Create({ account }) {
     }
   };
 
-  // async function createMint() {
-  // }
+  // const createSale = async (url) => {
+  //   try {
+  //     const response = await mintKallosTokenContract.methods
+  //       .mintKallosToken(url)
+  //       .send({ from: account });
+  //     console.log(response);
+  //   } catch (error) {
+  //     console.error(error);
+  //   }
+  // };
 
-  const createSale = async (url) => {
-    try {
-      const response = await mintKallosTokenContract.methods
-        .mintKallosToken(url)
-        .send({ from: account });
-
-      console.log(response);
-    } catch (error) {
-      console.error(error);
-    }
-  };
-
-  return (
+  return(
     <div>
       <Container>
-        <Typography variant="h3" sx={{ my: 8 }}>
+        <Typography variant="h3" sx={{ my : 8}}>
           NFT 등록하기
         </Typography>
         {/* <h1>NFT 등록하기</h1> */}
         <Box>
-          <Stack direction="row" sx={{ mt: 10 }}>
-            <Typography variant="h5" sx={{ mr: 14 }}>
-              작품 파일
-            </Typography>
-            <input
-              type="file"
-              name="Asset"
-              className="my-4"
-              onChange={onChange}
-            />
-            {fileUrl && (
-              <Image
-                className="rounded mt-4"
-                width="300px"
-                height="300px"
-                src={fileUrl}
-              />
-            )}
-
-            {/* <input
-              type="file"
-              accept="image/*"
+          <Stack direction="row" sx={{ mt : 10}}>
+            <Typography variant="h5" sx={{mr : 14}}>작품 파일</Typography>
+            {/* Box : 이미지 파일 미리보기 */}
+            <input type="file" accept="image/*"
               onChange={saveImage}
-              ref={(refParam) => (inputRef = refParam)}
+              ref={refParam => inputRef = refParam}
               style={{ display: "none" }}
             />
-            <div className="img-wrapper" style={{ cursor: "pointer" }}>
+            <div style={{ cursor:'pointer' }}>
               {loaded === false || loaded === true ? (
-                <Image
-                  src={image.preview_URL}
-                  alt="preview-image"
-                  width="350"
-                  height="350"
-                  onClick={() => inputRef.click()}
-                />
+                <Image src={image.preview_URL} alt="preview-image" width="350" height="350" onClick={() => inputRef.click()}/>
               ) : (
                 <span>이미지를 불러오는 중입니다.</span>
               )}
-            </div> */}
+            </div>
           </Stack>
-          <Stack direction="row" sx={{ my: 8 }}>
-            <Typography variant="h5" sx={{ mr: 7.7 }}>
-              디스크립션
-            </Typography>
-            <textarea
-              placeholder="Asset Description"
-              className="mt-2 border rounded p-4"
-              onChange={(e) =>
-                updateFormInput({ ...formInput, description: e.target.value })
-              }
-            />
+          <Stack direction="row" sx={{ my : 8}}>
+            <Typography variant="h5" sx={{mr : 7.7}}>작가명</Typography>
+            <TextField 
+              required 
+              variant="standard" 
+              sx={{ ml: 10, width: 700 }}
+              onChange={(e) => updateFormInput({ ...formInput, author : e.target.value})}
+              >
+            </TextField>
           </Stack>
           <Stack direction="row">
             <Typography variant="h5">제목(작품명)</Typography>
-            <input
-              placeholder="Asset Name"
-              className="mt-8 border rounded p-4"
-              onChange={(e) =>
-                updateFormInput({ ...formInput, title: e.target.value })
-              }
-            />
+            <TextField 
+              required 
+              variant="standard" 
+              sx={{ml : 10, width:700}}
+              onChange={(e) => updateFormInput({ ...formInput, title : e.target.value})}
+            >
+            </TextField>
           </Stack>
-          {/* <Stack direction="row" sx={{ my: 8 }}>
-            <Typography variant="h5">키워드(optional)</Typography>
-            <TextField
-              variant="standard"
-              sx={{ ml: 6, width: 700 }}
-            ></TextField>
-          </Stack> */}
+          <Stack direction="row" sx={{ my : 8}}>
+            <Typography variant="h5">설명</Typography>
+            <TextField 
+              required
+              variant="standard" 
+              sx={{ml : 6, width:700}}
+              onChange={(e) => updateFormInput({ ...formInput, description : e.target.value})}
+            >
+            </TextField>
+          </Stack>  
         </Box>
-        <Box sx={{ display: "flex", justifyContent: "flex-end" }}>
-          <Button onClick={createMint}>등록하기</Button>
-        </Box>
-
         {/* 버튼 끝으로 옮기기 */}
-        {/* <Box sx={{ display: 'flex', justifyContent: 'flex-end'}}>
-          <Button onClick={handleOpen} variant="contained" size="large" sx={{ mr : 28}}>등록하기</Button>
-          <Dialog 
-            open={open}
-            onClose={handleClose}
+        <Box sx={{ display: 'flex', justifyContent: 'flex-end'}}>
+          <Button 
+            onClick={createMint} 
+            variant="contained" 
+            size="large" 
+            sx={{ mr : 28}}
+            // onSubmit={ButtonCreate}
           >
-            <DialogContent sx={{ width : 400}}>
-              <h3>등록 승인하기</h3>
-              <TextField label="개인키 입력" sx={{ width : 350}}></TextField>
-            </DialogContent>
-            <DialogActions>
-              <Button>취소</Button>
-              <Button>승인하기</Button>
-            </DialogActions>
-          </Dialog>
-        </Box> */}
+            등록하기
+          </Button>
+        </Box>
       </Container>
     </div>
   );
