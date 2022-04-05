2e91012034c1eddea962ac68005d177e6ee98da8
김나영 <ny3021@gmail.con>
2022-04-05 22:33:20 +0900

    페이지네이션 수정

--- frontend/components/pagination.tsx
+++ frontend/components/pagination.tsx
@@ -1,23 +1,28 @@
-import React, {useState} from 'react';
+import React, { useState } from "react";
 import Pagination from "react-js-pagination";
-import styles from '../styles/Pagination.module.css';
+import styles from "../styles/Pagination.module.css";
 
 export interface ReactJsPaginationProps {
   activePage: number;
   itemsCountPerPage?: number | undefined;
   totalItemsCount: number;
   pageRangeDisplayed?: number | undefined;
-  prevPageText?: string| React.ReactElement | undefined;
+  prevPageText?: string | React.ReactElement | undefined;
   nextPageText?: string | React.ReactElement | undefined;
 }
 
 // totalCount : 총 아이템의 갯수(totalItemsCount), curPage : 현재 페이지
 // numPerPage : 한 페이지당 아이템 갯수
-const Paging = ({ curPage, totalItems, itemsPerPage, setCurPage}) => {
-
+const Paging = ({
+  curPage,
+  totalItems,
+  itemsPerPage,
+  setCurPage,
+  totalPages,
+}) => {
   const handlePageChange = (curPage) => {
-    setCurPage(curPage)
-  }
+    setCurPage(curPage);
+  };
 
   return (
     <div className={styles.container}>
@@ -25,13 +30,13 @@ const Paging = ({ curPage, totalItems, itemsPerPage, setCurPage}) => {
         activePage={curPage}
         itemsCountPerPage={itemsPerPage}
         totalItemsCount={totalItems}
-        pageRangeDisplayed={5}
+        pageRangeDisplayed={totalPages}
         prevPageText={"‹"}
         nextPageText={"›"}
         onChange={handlePageChange}
       />
     </div>
-  )
-}
+  );
+};
 
 export default Paging;
